80efe49b2a70b08c5f5f2b16dcd36b0b36404263
Dunc4nNT <76918336+Dunc4nNT@users.noreply.github.com>
2025-03-08 02:50:49 +0100

    move all html and css files into actual jinja templates

--- website/bit/tools.py
+++ website/bit/tools.py
@@ -1,3 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
-blueprint: Blueprint = Blueprint("tools", __name__, url_prefix="tools")
+blueprint: Blueprint = Blueprint("tools", __name__, url_prefix="/tools")
+
+
+@blueprint.get("/")
+def index() -> str:
+    """Tools landing page.
+
+    Returns
+    -------
+    str
+        The tools landing page template.
+    """
+    return render_template("tools/index.html")
