7cb55aeef54cd098c03cc77cf89b030cae083326
Sven Staats <svenstaats@outlook.com>
2025-03-13 14:42:23 +0100

    file upload now allows multiple files

--- website/bit/tools.py
+++ website/bit/tools.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
 blueprint: Blueprint = Blueprint("tools", __name__, url_prefix="/tools")
+
+allowed_extensions = ["fasta"]
 
 @blueprint.route("/", methods=["GET", "POST"])
 def index() -> str:
@@ -11,18 +13,30 @@ def index() -> str:
     str
         The tools landing page template.
     """
-    # default tool page, to upload file
+    # default tool page, to upload file(s)
     if request.method == "GET":
-        return render_template("tools/tools_GET.html")
+        return render_template("tools/tools_GET.html", allowed_extensions=allowed_extensions)
 
-    # if file is uploaded
+    # if submit button is pressed
     elif request.method == "POST":
-        file = request.files["file"]
-        filename = secure_filename(file.filename)  # SECURE filename
-        file_extension = filename.split(".")[-1]
-        # if no file uploaded, return to default tool page
-        if filename == "":
-            return render_template("tools/tools_GET.html")
-        if file_extension != "fasta":
-            return render_template("tools/tools_INVALID.html", file_extension=file_extension)
-        return render_template("tools/tools_POST.html", filename=filename)
+        files = request.files.getlist("files")
+
+        # validate files
+        for file in files:
+            # properties for each file
+            kwargs = {
+                "file": file,
+                "filename": secure_filename(file.filename),  # SECURE filename
+                "file_extension": file.filename.split(".")[-1],
+            }
+
+            # if no files given, return to default tool page
+            if kwargs["filename"] == "":
+                #return render_template("tools/tools_GET.html", allowed_extensions=allowed_extensions)
+                return redirect(url_for("tools.index"))
+
+            # if a file with an incorrect extension was given, return to tools_INVALID.html
+            if kwargs["file_extension"] not in allowed_extensions:
+                return render_template("tools/tools_INVALID.html", **kwargs)
+
+        return render_template("tools/tools_POST.html", files=files)
