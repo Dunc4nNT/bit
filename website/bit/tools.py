3137b903b69d54c0dd3892a7f315623369035a08
DESKTOP-TEONJLT\J.Veenstra <Johannaveenstra2004@hotmail.com>
2025-03-21 11:18:51 +0100

    Added max upload size and some errorhandling

--- website/bit/tools.py
+++ website/bit/tools.py
@@ -1,8 +1,12 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
+from werkzeug.exceptions import RequestEntityTooLarge
 blueprint: Blueprint = Blueprint("tools", __name__, url_prefix="/tools")
 
 allowed_extensions = ["fasta"]
+# sets max upload size to 16mb
+max_file_size = 16 * 1024 * 1024
+# max_file_size = 98_816
 
 @blueprint.route("/", methods=["GET", "POST"])
 def index() -> str:
@@ -28,6 +32,7 @@ def index() -> str:
                 "file": file,
                 "filename": secure_filename(file.filename),  # SECURE filename
                 "file_extension": file.filename.split(".")[-1],
+                "file_size": file.content_length,
             }
 
             # if no files given, return to default tool page
@@ -39,4 +44,13 @@ def index() -> str:
             if kwargs["file_extension"] not in allowed_extensions:
                 return render_template("tools/tools_INVALID.html", **kwargs)
 
-        return render_template("tools/tools_POST.html", files=files)
+            # if a file exceeds the size limit, return to tools_FILE_TOO_LARGE.html
+            if kwargs["file_size"] > max_file_size:
+                return render_template("tools/tools_FILE_TOO_LARGE.html", **kwargs)
+
+    return render_template("tools/tools_POST.html", files=files)
+
+@blueprint.errorhandler(RequestEntityTooLarge)
+def request_entity_too_large(error):
+    return render_template('tools/tools_FILE_TOO_LARGE.html'), 413
+
